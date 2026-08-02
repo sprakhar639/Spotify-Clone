bfc21a16937b2426a8dd761fa9648821b3f38e8d
sprakhar639 <sprakhar639@gmail.com>
2026-08-02 15:03:11 +0530

    registerUser and login Page

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,13 @@
 const userModel=require("../models/user.model");
-const jwt=require("process.env.JWT_URI")
+const jwt=require("jsonwebtoken")
+const bcrypt=require("bcryptjs")
 
 
 async function registerUser(req,res){
     const{username,email,password,role="user"}=req.body;
 
 
+    
     const isUserAlreadyExists=await userModel.findOne({
        $or:[
         {username},
@@ -18,14 +20,18 @@ if (isUserAlreadyExists){
     return res.status(409).json({message:"User already exists"})
 }
 
+const hash=await bcrypt.hash(password,10)
+
 const user=await userModel.create({
     username,
-    email,password,role
+    email,
+    password:hash,
+    role
 })
 
 
 const token=jwt.sign({
-   id= user._id,
+   id:user._id,
    role:user.role},
    process.env.JWT_SECRET)
 
@@ -35,11 +41,56 @@ const token=jwt.sign({
    res.status(201).json({
     message:"user registed successfully",
     user:{
-       id=user._id,
+       id:user._id,
  username: user.username,
  email: user.email,
- password: user.email,
+ password: user.password,
  role: user.role,
     }
    })
 }
+
+
+
+async function loginUser(req,res){
+    const {username,email,password}=req.body;
+    const user=await userModel.findOne({
+        $or:[
+           {username} ,
+           {email}
+        ]
+    })
+    if(!user){
+        return res.status(401).json({message:"Invalid Credentials"})
+    }
+
+
+
+    const isPasswordValid=await bcrypt.compare(password,user.password)
+
+
+
+    if(!isPasswordValid){
+        return res.status(401).json({message:"invalid credentials"})
+    }
+
+    const token=jwt.sign({
+       id:user._id,
+       role:user.role},
+       process.env.JWT_SECRET)
+    
+    res.cookie("token",token)
+
+    res.status(200).json({
+        message:"login successfully",
+        user:{
+            id:user._id,
+            username:user.username,
+            email:user.email,
+            role:user.role
+        }
+    })
+}
+
+
+module.exports={ registerUser,loginUser}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 const express=require('express');
+const authcontroller=require("../controllers/auth.controller")
 
 const router=express.Router();
 
-route.post('/register',)
+router.post('/register',authcontroller.registerUser)
+router.post('/login',authcontroller.loginUser)
 
 
 module.exports=router
